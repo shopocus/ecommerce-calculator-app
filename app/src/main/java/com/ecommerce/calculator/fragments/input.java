170a365d8e605cd04b218a886ffef4e4f3e31594
aashi gupta <aashigupta165@gmail.com>
2020-02-13 22:38:45 +0530

    implement validations on input fields and remove bugs on UI

--- app/src/main/java/com/ecommerce/calculator/fragments/input.java
+++ app/src/main/java/com/ecommerce/calculator/fragments/input.java
@@ -3,22 +3,31 @@ package com.ecommerce.calculator.fragments;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import androidx.core.content.ContextCompat;
+
+import android.os.Handler;
+import android.view.animation.LinearInterpolator;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Patterns;
+import com.ecommerce.calculator.models.progressButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.core.widget.ContentLoadingProgressBar;
 import androidx.fragment.app.Fragment;
 import com.ecommerce.calculator.models.SaveResponse;
 import com.ecommerce.calculator.activities.MainActivity;
@@ -44,13 +53,20 @@ public class input extends Fragment implements View.OnClickListener {
     private static final String TAG = "input";
 
     private ImageButton details, expenses, discounts, save, sendEmail;
-    private TextView pp, gst, transport, packaging, labour, storage, other, price, percentage, line1, line2, line3, line4, line5, line6, textViewResult;
-    private EditText num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+    private TextView pp, gst, transport, packaging, labour, storage, other, price, percentage, line1, line2, line3, line4, line5, line6,rs1,rs2,rs3,rs4,rs5,rs6,rs7,rs8,rs9;
+    private EditText num1, num2, num4, num5, num6, num7, num8, num9, num10;
 
+    Spinner num3;
     ListView itemList;
     CardView result_card;
     Double[] items = new Double[8] ;
     private String myText;
+    String gst_array[] = {"0", "5", "12", "18", "28"};
+    String spinner_ans;
+    //progressButton progressButton;
+    private ProgressBar progressBar;
+    private TextView textView;
+    //ProgressBar progressBar;
    // output text1,text2,text3,text4,text5,text6,text7,text8;
 
     @Nullable
@@ -69,15 +85,19 @@ public class input extends Fragment implements View.OnClickListener {
             {
                 if (pp.getVisibility() == View.GONE) {
                     pp.setVisibility(View.VISIBLE);
+                    rs1.setVisibility(View.VISIBLE);
                     num2.setVisibility(View.VISIBLE);
                     gst.setVisibility(View.VISIBLE);
+                    rs2.setVisibility(View.VISIBLE);
                     num3.setVisibility(View.VISIBLE);
                     line1.setVisibility(View.VISIBLE);
                     details.setRotation(180);
                 } else {
                     pp.setVisibility(View.GONE);
+                    rs1.setVisibility(View.GONE);
                     num2.setVisibility(View.GONE);
                     gst.setVisibility(View.GONE);
+                    rs2.setVisibility(View.GONE);
                     num3.setVisibility(View.GONE);
                     line1.setVisibility(View.GONE);
                     details.setRotation(0);
@@ -91,14 +111,19 @@ public class input extends Fragment implements View.OnClickListener {
             public void onClick(View v) {
                 if (transport.getVisibility() == View.GONE) {
                     transport.setVisibility(View.VISIBLE);
+                    rs3.setVisibility(View.VISIBLE);
                     num4.setVisibility(View.VISIBLE);
                     packaging.setVisibility(View.VISIBLE);
+                    rs4.setVisibility(View.VISIBLE);
                     num5.setVisibility(View.VISIBLE);
                     labour.setVisibility(View.VISIBLE);
+                    rs5.setVisibility(View.VISIBLE);
                     num6.setVisibility(View.VISIBLE);
                     storage.setVisibility(View.VISIBLE);
+                    rs6.setVisibility(View.VISIBLE);
                     num7.setVisibility(View.VISIBLE);
                     other.setVisibility(View.VISIBLE);
+                    rs7.setVisibility(View.VISIBLE);
                     num8.setVisibility(View.VISIBLE);
                     line2.setVisibility(View.VISIBLE);
                     line3.setVisibility(View.VISIBLE);
@@ -107,14 +132,19 @@ public class input extends Fragment implements View.OnClickListener {
                     expenses.setRotation(180);
                 } else {
                     transport.setVisibility(View.GONE);
+                    rs3.setVisibility(View.GONE);
                     num4.setVisibility(View.GONE);
                     packaging.setVisibility(View.GONE);
+                    rs4.setVisibility(View.GONE);
                     num5.setVisibility(View.GONE);
                     labour.setVisibility(View.GONE);
+                    rs5.setVisibility(View.GONE);
                     num6.setVisibility(View.GONE);
                     storage.setVisibility(View.GONE);
+                    rs6.setVisibility(View.GONE);
                     num7.setVisibility(View.GONE);
                     other.setVisibility(View.GONE);
+                    rs7.setVisibility(View.GONE);
                     num8.setVisibility(View.GONE);
                     line2.setVisibility(View.GONE);
                     line3.setVisibility(View.GONE);
@@ -133,15 +163,19 @@ public class input extends Fragment implements View.OnClickListener {
             {
                 if (price.getVisibility() == View.GONE) {
                     price.setVisibility(View.VISIBLE);
+                    rs8.setVisibility(View.VISIBLE);
                     num9.setVisibility(View.VISIBLE);
                     percentage.setVisibility(View.VISIBLE);
+                    rs9.setVisibility(View.VISIBLE);
                     num10.setVisibility(View.VISIBLE);
                     line6.setVisibility(View.VISIBLE);
                     discounts.setRotation(180);
                 } else {
                     price.setVisibility(View.GONE);
+                    rs7.setVisibility(View.GONE);
                     num9.setVisibility(View.GONE);
                     percentage.setVisibility(View.GONE);
+                    rs8.setVisibility(View.GONE);
                     num10.setVisibility(View.GONE);
                     line6.setVisibility(View.GONE);
                     discounts.setRotation(0);
@@ -149,13 +183,36 @@ public class input extends Fragment implements View.OnClickListener {
             }
         });
 
-        Button calculate = view.findViewById(R.id.calculate);
+        View calculate = view.findViewById(R.id.calculate);
         calculate.setOnClickListener(new OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
+                progressButton progressButton = new progressButton(getActivity(), v);
+                progressButton.ButtonActivated();
                calculate();
+            }
+        });
+
+        ImageButton reset = view.findViewById(R.id.reset);
+        reset.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                num1.requestFocus();
+                num1.setText("");
+                num2.setText("");
+                num3.setSelection(0);
+                num4.setText("");
+                num5.setText("");
+                num6.setText("");
+                num7.setText("");
+                num8.setText("");
+                num9.setText("");
+                num10.setText("");
+                result_card.setVisibility(View.GONE);
             }
         });
 
@@ -221,6 +278,15 @@ public class input extends Fragment implements View.OnClickListener {
             line4 = view.findViewById(R.id.line4);
             line5 = view.findViewById(R.id.line5);
             line6 = view.findViewById(R.id.line6);
+            rs1 = view.findViewById(R.id.rs1);
+            rs2 = view.findViewById(R.id.rs2);
+            rs3 = view.findViewById(R.id.rs3);
+            rs4 = view.findViewById(R.id.rs4);
+            rs5 = view.findViewById(R.id.rs5);
+            rs6 = view.findViewById(R.id.rs6);
+            rs7 = view.findViewById(R.id.rs7);
+            rs8 = view.findViewById(R.id.rs8);
+            rs9 = view.findViewById(R.id.rs9);
             expenses = view.findViewById(R.id.expenses_dropdown);
             discounts = view.findViewById(R.id.discounts_dropdown);
             price = view.findViewById(R.id.text_price);
@@ -228,6 +294,25 @@ public class input extends Fragment implements View.OnClickListener {
             num1 = view.findViewById(R.id.number1);
             num2 = view.findViewById(R.id.number2);
             num3 = view.findViewById(R.id.number3);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_activated_1, gst_array);
+// Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_list_item_activated_1);
+            num3.setAdapter(adapter);
+            num3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            spinner_ans = gst_array[position];
+                          //  Log.d(TAG,spinner_ans);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+// Apply the adapter to the spinner
+           // num3.setAdapter(adapter);
             num4 = view.findViewById(R.id.number4);
             num5 = view.findViewById(R.id.number5);
             num6 = view.findViewById(R.id.number6);
@@ -235,6 +320,9 @@ public class input extends Fragment implements View.OnClickListener {
             num8 = view.findViewById(R.id.number8);
             num9 = view.findViewById(R.id.number9);
             num10 = view.findViewById(R.id.number10);
+            progressBar = view.findViewById(R.id.loader);
+            textView = view.findViewById(R.id.calculate_textview);
+           // progressBar = (ProgressBar)view.findViewById(R.id.spin_kit);
            // textViewResult = view.findViewById(R.id.text_view_resulta);
 
            // view.findViewById(R.id.reset).setOnClickListener(this);
@@ -242,9 +330,60 @@ public class input extends Fragment implements View.OnClickListener {
         }
 
     protected void calculate() {
+
+        if (num1.getText().toString().isEmpty()) {
+            num1.setError("Selling Price is required");
+            num1.requestFocus();
+            ButtonFinished();
+            return;
+        }
+
+        if (num2.getText().toString().isEmpty()) {
+            num2.setError("Purchase Price is required");
+            num2.requestFocus();
+            ButtonFinished();
+            return;
+        }
+
+        if(Double.parseDouble(num1.getText().toString()) <= 0){
+            num1.setError("Selling Price not valid");
+            num1.requestFocus();
+            ButtonFinished();
+            return;
+        }
+
+        if(Double.parseDouble(num2.getText().toString()) <= 0){
+            num2.setError("Purchase Price is not valid");
+            num2.requestFocus();
+            ButtonFinished();
+            return;
+        }
+
+        if(num4.getText().toString().isEmpty())
+            num4.setText("0");
+        if(num5.getText().toString().isEmpty())
+            num5.setText("0");
+        if(num6.getText().toString().isEmpty())
+            num6.setText("0");
+        if(num7.getText().toString().isEmpty())
+            num7.setText("0");
+        if(num8.getText().toString().isEmpty())
+            num8.setText("0");
+        if(num9.getText().toString().isEmpty())
+            num9.setText("0");
+        if(num10.getText().toString().isEmpty())
+            num10.setText("0");
+
+        if (Double.parseDouble(num9.getText().toString()) > 0 && Double.parseDouble(num10.getText().toString()) > 0) {
+            num9.setError("One discount criteria valid at a time");
+            num9.requestFocus();
+            ButtonFinished();
+            return;
+        }
+
         double number1 = Double.parseDouble(num1.getText().toString());
         double number2 = Double.parseDouble(num2.getText().toString());
-        double number3 = Double.parseDouble(num3.getText().toString());
+        double number3 = Double.parseDouble(spinner_ans);
         double number4 = Double.parseDouble(num4.getText().toString());
         double number5 = Double.parseDouble(num5.getText().toString());
         double number6 = Double.parseDouble(num6.getText().toString());
@@ -261,6 +400,9 @@ public class input extends Fragment implements View.OnClickListener {
             public void onResponse(Call<CalculateResponse> call, Response<CalculateResponse> response) {
                 CalculateResponse CalculateResponse = response.body();
                 Toast.makeText(getActivity(), "result", Toast.LENGTH_LONG).show();
+
+                ButtonFinished();
+
                 result_card.setVisibility(View.VISIBLE);
 
                 items[0] = CalculateResponse.getBankSettlement();
@@ -293,6 +435,11 @@ public class input extends Fragment implements View.OnClickListener {
 
                 OutputListAdapter adapter = new OutputListAdapter(getActivity(), R.layout.output_row, outputList);
                 itemList.setAdapter(adapter);
+
+                //progressButton.ButtonFinished();
+
+                save.setImageResource(R.drawable.ic_bookmark_border);
+                save.setEnabled(true);
 //
 //                SharedPrefManager.getInstance(getActivity())
 //                        .saveResult(text1.getAnswer(),text2.getAnswer(),text3.getAnswer(),text4.getAnswer(),text5.getAnswer(),text6.getAnswer(),text7.getAnswer(),text8.getAnswer());
@@ -328,7 +475,7 @@ public class input extends Fragment implements View.OnClickListener {
         String email = SharedPrefManager.getInstance(getActivity()).getUser().getEmail();
         String title = String.valueOf(myText);
         String sellingPrice = num1.getText().toString().trim();
-        String gstOnProduct = num3.getText().toString().trim();
+        String gstOnProduct = spinner_ans;
         String productPriceWithoutGst = num2.getText().toString().trim();
         String inwardShipping = num4.getText().toString().trim();
         String packagingExpense = num5.getText().toString().trim();
@@ -362,6 +509,8 @@ public class input extends Fragment implements View.OnClickListener {
             public void onResponse(Call<SaveResponse> call, Response<SaveResponse> response) {
                 SaveResponse dr = response.body();
                 //if (dr.getEmail().equals("yes")) {
+                save.setImageResource(R.drawable.ic_bookmark);
+                save.setEnabled(false);
                     Toast.makeText(getActivity(), dr.getEmail(), Toast.LENGTH_LONG).show();
                     //SharedPrefManager.getInstance(getActivity())
                       //      .saveUser(dr.getEmail());
@@ -386,24 +535,26 @@ public class input extends Fragment implements View.OnClickListener {
 
     public void share(){
 
-        String content = "sellingPrice" + num1.getText().toString().trim() + "\n" +
-        "gstOnProduct" + num3.getText().toString().trim() + "\n" +
-        "productPriceWithoutGst" + num2.getText().toString().trim() + "\n" +
-        "inwardShipping" + num4.getText().toString().trim() + "\n" +
-        "packagingExpense" + num5.getText().toString().trim() + "\n" +
-        "labour" + num6.getText().toString().trim() + "\n" +
-        "storageFee" + num7.getText().toString().trim() + "\n" +
-        "other" + num8.getText().toString().trim() + "\n" +
-        "discountPercent" + num10.getText().toString().trim() + "\n" +
-        "discountAmount" + num9.getText().toString().trim() + "\n" +
-        "bankSettlement" + String.valueOf(items[0]) + "\n" +
-        "totalMeeshoCommision" + String.valueOf(items[1]) + "\n" +
-        "profit" + String.valueOf(items[2]) + "\n" +
-                "totalGstPayable" + String.valueOf(items[3]) + "\n" +
-        "tcs" + String.valueOf(items[4]) + "\n" +
-        "gstPayable" + String.valueOf(items[5]) + "\n" +
-        "gstClaim" + String.valueOf(items[6]) + "\n" +
-        "profitPercentage" + String.valueOf(items[7]);
+        String content = "INPUT" + "\n" +
+                "Selling Price: " + num1.getText().toString().trim() + "\n" +
+        "GST On Product: " + spinner_ans + "\n" +
+        "Product Price Without GST: " + num2.getText().toString().trim() + "\n" +
+        "Inward Shipping: " + num4.getText().toString().trim() + "\n" +
+        "Packaging Expense: " + num5.getText().toString().trim() + "\n" +
+        "Labour: " + num6.getText().toString().trim() + "\n" +
+        "Storage Fee: " + num7.getText().toString().trim() + "\n" +
+        "Other: " + num8.getText().toString().trim() + "\n" +
+        "Discount Percent: " + num10.getText().toString().trim() + "\n" +
+        "Discount Amount: " + num9.getText().toString().trim() + "\n\n" +
+                "OUTPUT" + "\n" +
+        "Bank Settlement: " + String.valueOf(items[0]) + "\n" +
+        "Total Meesho Commision: " + String.valueOf(items[1]) + "\n" +
+        "Profit: " + String.valueOf(items[2]) + "\n" +
+                "Total GST Payable: " + String.valueOf(items[3]) + "\n" +
+        "Tcs: " + String.valueOf(items[4]) + "\n" +
+        "GST Payable: " + String.valueOf(items[5]) + "\n" +
+        "GST Claim: " + String.valueOf(items[6]) + "\n" +
+        "Profit Percentage: " + String.valueOf(items[7]);
 
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
@@ -412,6 +563,11 @@ public class input extends Fragment implements View.OnClickListener {
         intent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
         intent.putExtra(intent.EXTRA_TEXT, shareBody);
         startActivity(Intent.createChooser(intent, "Share using"));
+    }
+
+    void ButtonFinished(){
+        progressBar.setVisibility(View.GONE);
+        textView.setText("Calculate");
     }
 
     @Override
