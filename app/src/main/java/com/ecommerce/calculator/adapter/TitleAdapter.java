a7694e603180ea9850d155bbe18357b2bf1fb06c
aashi gupta <aashigupta165@gmail.com>
2020-03-12 14:27:45 +0530

    add option menu for delete and rename

--- app/src/main/java/com/ecommerce/calculator/adapter/TitleAdapter.java
+++ app/src/main/java/com/ecommerce/calculator/adapter/TitleAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.text.InputType;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +25,7 @@ import com.ecommerce.calculator.models.DeleteDataResponse;
 import com.ecommerce.calculator.storage.SharedPrefManager;
 import android.widget.Filter;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import android.view.ViewGroup;
@@ -44,15 +46,16 @@ public class TitleAdapter extends RecyclerView.Adapter<TitleAdapter.TitleHolder>
     FragmentManager fragmentManager;
 
     public static class TitleHolder extends RecyclerView.ViewHolder {
-        public TextView textViewTitle;
-        public ImageButton delete;
-        public Button edit_button;
+        public TextView textViewTitle,optionMenu;
+       // public ImageButton delete;
+       // public Button edit_button;
 
         public TitleHolder(View itemView) {
             super(itemView);
             textViewTitle = itemView.findViewById(R.id.title);
-            delete = itemView.findViewById(R.id.delete);
-            edit_button = itemView.findViewById(R.id.edit_button);
+            optionMenu = itemView.findViewById(R.id.optionMenu);
+            //delete = itemView.findViewById(R.id.delete);
+            //edit_button = itemView.findViewById(R.id.edit_button);
         }
     }
 
@@ -79,10 +82,9 @@ public class TitleAdapter extends RecyclerView.Adapter<TitleAdapter.TitleHolder>
         holder.textViewTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(newTitle[0].equals("")){
+                if (newTitle[0].equals("")) {
                     SharedPrefManager.getInstance(mCtx).saveTitle(title);
-                }
-                else {
+                } else {
                     SharedPrefManager.getInstance(mCtx).saveTitle(newTitle[0]);
                 }
                 Log.d(Tag, title);
@@ -91,105 +93,115 @@ public class TitleAdapter extends RecyclerView.Adapter<TitleAdapter.TitleHolder>
             }
         });
 
-        holder.delete.setOnClickListener(new View.OnClickListener() {
+        holder.optionMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(mCtx);
-                builder.setMessage("Are you sure you want to Delete?")
-                        .setCancelable(false)
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                String email = SharedPrefManager.getInstance(mCtx).getUser().getEmail();
-                                Call<DeleteDataResponse> call = RetrofitClient.getInstance().getApi().DeleteData(email, title);
 
-                                call.enqueue(new Callback<DeleteDataResponse>() {
-                                    @Override
-                                    public void onResponse(Call<DeleteDataResponse> call, Response<DeleteDataResponse> response) {
-                                        DeleteDataResponse deleteDataResponse = response.body();
+                PopupMenu popupMenu = new PopupMenu(mCtx, holder.optionMenu);
+                popupMenu.inflate(R.menu.option_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.rename:
+                                final EditText edittitle = new EditText(mCtx);
+                                edittitle.setInputType(InputType.TYPE_CLASS_TEXT);
+                                edittitle.setHint("Enter new Title");
+                                edittitle.requestFocus();
+                                edittitle.setPadding(70, 40, 50, 40);
+                                androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(mCtx);
+                                builder.setTitle("New Title")
+                                        .setCancelable(false)
+                                        .setView(edittitle)
+                                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                newTitle[0] = edittitle.getText().toString();
+                                                if (newTitle[0].equals("")) {
+                                                    Toast.makeText(mCtx, "Invalid Title", Toast.LENGTH_LONG).show();
+                                                } else {
+                                                    String email = SharedPrefManager.getInstance(mCtx).getUser().getEmail();
+                                                    Call<DeleteDataResponse> call = RetrofitClient.getInstance().getApi().update(email, title, newTitle[0]);
 
-                                        if (deleteDataResponse.getMessage().equals("deleted")) {
-                                            titleList.remove(position);
-                                            notifyItemRemoved(position);
-                                            Toast.makeText(mCtx, "Deleted", Toast.LENGTH_LONG).show();
-                                        }
-                                    }
+                                                    call.enqueue(new Callback<DeleteDataResponse>() {
+                                                        @Override
+                                                        public void onResponse(Call<DeleteDataResponse> call, Response<DeleteDataResponse> response) {
+                                                            DeleteDataResponse deleteDataResponse = response.body();
 
-                                    @Override
-                                    public void onFailure(Call<DeleteDataResponse> call, Throwable t) {
-                                        Toast.makeText(mCtx, "Internet Disconnected", Toast.LENGTH_LONG).show();
-                                    }
-                                });
-                            }
-                        })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                            }
-                        });
-                androidx.appcompat.app.AlertDialog alertDialog = builder.create();
-                alertDialog.show();
-
-            }
-        });
-
-        holder.edit_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final EditText edittitle = new EditText(mCtx);
-                edittitle.setInputType(InputType.TYPE_CLASS_TEXT);
-                edittitle.setHint("Enter new Title");
-                edittitle.requestFocus();
-                edittitle.setPadding(70,40,50,40);
-                androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(mCtx);
-                builder.setTitle("New Title")
-                        .setCancelable(false)
-                        .setView(edittitle)
-                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                newTitle[0] = edittitle.getText().toString();
-                                if (newTitle[0].equals("")) {
-                                    Toast.makeText(mCtx, "Invalid Title", Toast.LENGTH_LONG).show();
-                                } else {
-                                    String email = SharedPrefManager.getInstance(mCtx).getUser().getEmail();
-                                    Call<DeleteDataResponse> call = RetrofitClient.getInstance().getApi().update(email, title, newTitle[0]);
-
-                                    call.enqueue(new Callback<DeleteDataResponse>() {
-                                        @Override
-                                        public void onResponse(Call<DeleteDataResponse> call, Response<DeleteDataResponse> response) {
-                                            DeleteDataResponse deleteDataResponse = response.body();
-
-                                            if (deleteDataResponse.getMessage().equals("title_updated")) {
-                                               // TitleAdapter.this.notifyItemChanged(position);
-                                                holder.textViewTitle.setText(newTitle[0]);
+                                                            if (deleteDataResponse.getMessage().equals("title_updated")) {
+                                                                // TitleAdapter.this.notifyItemChanged(position);
+                                                                holder.textViewTitle.setText(newTitle[0]);
 //                                                title.replace(title,newTitle);
 //                                                holder.textViewTitle.setText(title);
-                                                //super.onBindViewHolder(holder,position);
-                                                //title.replace(title,newTitle);\
-                                               // titleList.set(position,newTitle);
-                                                Toast.makeText(mCtx, "Updated", Toast.LENGTH_LONG).show();
-                                            }
-                                        }
+                                                                //super.onBindViewHolder(holder,position);
+                                                                //title.replace(title,newTitle);\
+                                                                // titleList.set(position,newTitle);
+                                                                Toast.makeText(mCtx, "Updated", Toast.LENGTH_LONG).show();
+                                                            }
+                                                        }
 
-                                        @Override
-                                        public void onFailure(Call<DeleteDataResponse> call, Throwable t) {
-                                            Toast.makeText(mCtx, "Internet Disconnected", Toast.LENGTH_LONG).show();
-                                        }
-                                    });
-                                   // update(title, newTitle);
-                                }
-                            }
-                        })
-                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                            }
-                        });
-                androidx.appcompat.app.AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+                                                        @Override
+                                                        public void onFailure(Call<DeleteDataResponse> call, Throwable t) {
+                                                            Toast.makeText(mCtx, "Internet Disconnected", Toast.LENGTH_LONG).show();
+                                                        }
+                                                    });
+                                                    // update(title, newTitle);
+                                                }
+                                            }
+                                        })
+                                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.cancel();
+                                            }
+                                        });
+                                androidx.appcompat.app.AlertDialog alertDialog = builder.create();
+                                alertDialog.show();
+                                break;
+
+                            case R.id.delete:
+                                androidx.appcompat.app.AlertDialog.Builder builder1 = new androidx.appcompat.app.AlertDialog.Builder(mCtx);
+                                builder1.setMessage("Are you sure you want to Delete?")
+                                        .setCancelable(false)
+                                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                String email = SharedPrefManager.getInstance(mCtx).getUser().getEmail();
+                                                Call<DeleteDataResponse> call = RetrofitClient.getInstance().getApi().DeleteData(email, title);
+
+                                                call.enqueue(new Callback<DeleteDataResponse>() {
+                                                    @Override
+                                                    public void onResponse(Call<DeleteDataResponse> call, Response<DeleteDataResponse> response) {
+                                                        DeleteDataResponse deleteDataResponse = response.body();
+
+                                                        if (deleteDataResponse.getMessage().equals("deleted")) {
+                                                            titleList.remove(position);
+                                                            notifyItemRemoved(position);
+                                                            Toast.makeText(mCtx, "Deleted", Toast.LENGTH_LONG).show();
+                                                        }
+                                                    }
+
+                                                    @Override
+                                                    public void onFailure(Call<DeleteDataResponse> call, Throwable t) {
+                                                        Toast.makeText(mCtx, "Internet Disconnected", Toast.LENGTH_LONG).show();
+                                                    }
+                                                });
+                                            }
+                                        })
+                                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.cancel();
+                                            }
+                                        });
+                                androidx.appcompat.app.AlertDialog alertDialog1 = builder1.create();
+                                alertDialog1.show();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
             }
         });
     }
