851da64c0e39cf0768bff6530ed0607ec610c978
aashi gupta <aashigupta165@gmail.com>
2020-03-24 19:38:29 +0530

    layout for email verification through otp

--- app/src/main/java/com/ecommerce/calculator/activities/MainActivity.java
+++ app/src/main/java/com/ecommerce/calculator/activities/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
 
         editTextName = findViewById(R.id.editTextName);
         editTextEmail = findViewById(R.id.editTextEmail);
