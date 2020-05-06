8d67d18c5f75ee94158bdca044f51a6fac9bbcdd
aashi gupta <aashigupta165@gmail.com>
2020-05-06 19:20:00 +0530

    club factory api added to retrofit

--- app/src/main/java/com/ecommerce/calculator/api/Api.java
+++ app/src/main/java/com/ecommerce/calculator/api/Api.java
@@ -79,6 +79,88 @@ public interface Api {
     );
 
     @FormUrlEncoded
+    @POST("clubFactory/app/calculate")
+    Call<CalculateResponse> clubFactoyrCalculation(
+            @Field("category") String category,
+            @Field("sellingPrice") double sellingPrice,
+            @Field("gstOnProduct") double gstOnProduct,
+            @Field("productPriceWithoutGst") double productPriceWithoutGst,
+            @Field("inwardShipping") double inwardShipping,
+            @Field("packagingExpense") double packagingExpense,
+            @Field("labour") double labour,
+            @Field("storageFee") double storageFee,
+            @Field("other") double other,
+            @Field("discountPercent") double discountPercent,
+            @Field("discountAmount") double discountAmount,
+            @Field("weight") double weight,
+            @Field("payMode") String payMode,
+            @Field("courier") String courier
+    );
+
+    @GET("clubFactory/app/showCategory")
+    Call<category> getCategoriesClubFactory( );
+
+    @FormUrlEncoded
+    @POST("clubFactory/app/toSave")
+    Call<SaveResponse> savedclubFactory(
+            @Field("title") String title,
+            @Field("category") String category,
+            @Field("sellingPrice") String sellingPrice,
+            @Field("gstOnProduct") String gstOnProduct,
+            @Field("productPriceWithoutGst") String productPriceWithoutGst,
+            @Field("inwardShipping") String inwardShipping,
+            @Field("packagingExpense") String packagingExpense,
+            @Field("labour") String labour,
+            @Field("storageFee") String storageFee,
+            @Field("other") String other,
+            @Field("discountPercent") String discountPercent,
+            @Field("discountAmount") String discountAmount,
+            @Field("weight") String weight,
+            @Field("payMode") String payMode,
+            @Field("courier") String courier,
+            @Field("bankSettlementLocal") String bankSettlementLocal,
+            @Field("totalMeeshoCommisionLocal") String totalMeeshoCommisionLocal,
+            @Field("totalGstPayableLocal") String totalGstPayableLocal,
+            @Field("tcsLocal") String tcsLocal,
+            @Field("gstPayableLocal") String gstPayableLocal,
+            @Field("gstClaimLocal") String gstClaimLocal,
+            @Field("profitLocal") String profitLocal,
+            @Field("profitPercentageLocal") String profitPercentageLocal,
+            @Field("bankSettlementRegional") String bankSettlementRegional,
+            @Field("totalCfCommisionRegional") String totalCfCommisionRegional,
+            @Field("totalGstPayableRegional") String totalGstPayableRegional,
+            @Field("tcsRegional") String tcsRegional,
+            @Field("gstPayableRegional") String gstPayableRegional,
+            @Field("gstClaimRegional") String gstClaimRegional,
+            @Field("profitRegional") String profitRegional,
+            @Field("profitPercentageRegional") String profitPercentageRegional,
+            @Field("bankSettlementMetro") String bankSettlementMetro,
+            @Field("totalCfCommisionMetro") String totalMeeshoCommisionMetro,
+            @Field("totalGstPayableMetro") String totalGstPayableMetro,
+            @Field("tcsMetro") String tcsMetro,
+            @Field("gstPayableMetro") String gstPayableMetro,
+            @Field("gstClaimMetro") String gstClaimMetro,
+            @Field("profitMetro") String profitMetro,
+            @Field("profitPercentageMetro") String profitPercentageMetro,
+            @Field("bankSettlementRestOfIndia") String bankSettlementRestOfIndia,
+            @Field("totalCfCommisionRestOfIndia") String totalMeeshoCommisionRestOfIndia,
+            @Field("totalGstPayableRestOfIndia") String totalGstPayableRestOfIndia,
+            @Field("tcsRestOfIndia") String tcsRestOfIndia,
+            @Field("gstPayableRestOfIndia") String gstPayableRestOfIndia,
+            @Field("gstClaimRestOfIndia") String gstClaimRestOfIndia,
+            @Field("profitRestOfIndia") String profitRestOfIndia,
+            @Field("profitPercentageRestOfIndia") String profitPercentageRestOfIndia,
+            @Field("bankSettlementKerela") String bankSettlementKerela,
+            @Field("totalMeeshoCommisionKerela") String totalMeeshoCommisionKerela,
+            @Field("totalGstPayableKerela") String totalGstPayableKerela,
+            @Field("tcsKerela") String tcsKerela,
+            @Field("gstPayableKerela") String gstPayableKerela,
+            @Field("gstClaimKerela") String gstClaimKerela,
+            @Field("profitKerela") String profitKerela,
+            @Field("profitPercentageKerela") String profitPercentageKerala
+    );
+
+    @FormUrlEncoded
     @POST("common/show/saved/title")
     Call<savedTitleResponse> getTitles(
             @Field("company") String company
