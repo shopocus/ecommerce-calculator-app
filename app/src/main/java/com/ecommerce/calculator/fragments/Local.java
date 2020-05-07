596c0e1f770daed271b79cf23bc2c89ef9520dec
aashi gupta <aashigupta165@gmail.com>
2020-05-07 22:27:19 +0530

    output fragments for club factory

--- app/src/main/java/com/ecommerce/calculator/fragments/Local.java
+++ app/src/main/java/com/ecommerce/calculator/fragments/Local.java
@@ -7,59 +7,52 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.ecommerce.calculator.R;
+import com.ecommerce.calculator.adapter.OutputListAdapter;
+import com.ecommerce.calculator.models.output;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Local#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class Local extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Local() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Local.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Local newInstance(String param1, String param2) {
-        Local fragment = new Local();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    ListView itemList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_local, container, false);
+        View view = inflater.inflate(R.layout.fragment_local, container, false);
+        itemList = view.findViewById(R.id.text_view_result);
+
+        output text1 = new output("Bank Settlement", String.valueOf(0));
+        output text2 = new output("Total Meesho Commision", String.valueOf(0));
+        output text3 = new output("Profit", String.valueOf(0));
+        output text4 = new output("Total GST Payable", String.valueOf(0));
+        output text5 = new output("TCS", String.valueOf(0));
+        output text6 = new output("GST Payable", String.valueOf(0));
+        output text7 = new output("GST Claim", String.valueOf(0));
+        output text8 = new output("Profit Percentage", String.valueOf(0));
+
+        ArrayList<output> outputList = new ArrayList<>();
+        outputList.add(text1);
+        outputList.add(text2);
+        outputList.add(text3);
+        outputList.add(text4);
+        outputList.add(text5);
+        outputList.add(text6);
+        outputList.add(text7);
+        outputList.add(text8);
+
+        OutputListAdapter adapter = new OutputListAdapter(getActivity(), R.layout.output_row, outputList);
+        itemList.setAdapter(adapter);
+        return view;
     }
 }
